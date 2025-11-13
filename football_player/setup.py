6bff1fd51e91123aea64b437de1d5f53b3f2c3a2
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-11-13 20:14:12 -0300

    modificación mantainer

--- football_player/setup.py
+++ football_player/setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ijo_del_estiven',
+    maintainer='ijo_el_estiven',
     maintainer_email='phillipe.joos@ug.uchile.cl',
     description='TODO: Package description',
     license='TODO: License declaration',
