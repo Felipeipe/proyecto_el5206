ac5e1da3332523a0f79515c14220a80e25b22a32
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-11-13 20:43:10 -0300

    se agregan parámetros para facilidad de tuneo de controlador

--- football_player/football_player/follow_and_avoid.py
+++ football_player/football_player/follow_and_avoid.py
@@ -17,14 +17,41 @@ class FollowAndAvoid(Node):
             10,
         )
 
+        self.declare_parameter('ball_proportional_gain',    1.0)
+        self.declare_parameter('ball_integral_gain',        1.0)
+        self.declare_parameter('ball_derivative_gain',      1.0)
+        self.declare_parameter('person_proportional_gain',  1.0)
+        self.declare_parameter('person_integral_gain',      1.0)
+        self.declare_parameter('person_derivative_gain',    1.0)
+        self.declare_parameter('thresh_before_turning',   10000)
+
+        ball_K_p = self.get_parameter('ball_proportional_gain').value
+        ball_K_i = self.get_parameter('ball_integral_gain').value
+        ball_K_d = self.get_parameter('ball_derivative_gain').value
+        person_K_p = self.get_parameter('person_proportional_gain').value
+        person_K_i = self.get_parameter('person_integral_gain').value
+        person_K_d = self.get_parameter('person_derivative_gain').value
+        turning_thresh = self.get_parameter('thresh_before_turning').value
+
     def listener_callback(self, msg: Detection2DArray):
         vel = Twist()
         for detection in msg.detections:
             for results in detection.results:
                 label = self.id_parser(results.id)
-                
-                
-                
+                if label == 'ball':
+                    pass
+                elif label == 'person':
+                    pass 
+                else:
+                    pass
+    def human_commands(self, pose):
+        linear_vel, angular_vel = (0,0)
+        return linear_vel, angular_vel
+
+    def ball_commands(self, pose):
+        linear_vel, angular_vel = (0,0)
+        return linear_vel, angular_vel
+        
     def id_parser(self, val):
         if val == 1:
             return 'ball'
---
+++ football_player/launch/football_player.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    pkg_name = 'football_player'
+    config_path = PathJoinSubstitution([
+        FindPackageShare(pkg_name),
+        "params",
+        "controller_gains.yaml"
+    ])
+    controller_node = Node(
+        package=pkg_name,
+        executable="follow_and_avoid",
+        name="follower_and_avoider",
+        output="screen",
+        parameters=[config_path]
+    )
+    detection_and_estimation_node = Node(
+        package=pkg_name,
+        executable="detector",
+        name="detector_node",
+        output="screen"
+    )
+    return LaunchDescription([
+        controller_node,
+        detection_and_estimation_node
+    ])
