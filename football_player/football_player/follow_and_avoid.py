84fca3e66e2752a0623aad795e43d26b909ccc82
TomasStevenson <101475971+TomasStevenson@users.noreply.github.com>
2025-11-17 13:17:09 -0300

    control Proporcional bih

--- football_player/football_player/follow_and_avoid.py
+++ football_player/football_player/follow_and_avoid.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from vision_msgs.msg import Detection2DArray
 from geometry_msgs.msg import Twist
 
+
 class FollowAndAvoid(Node):
     def __init__(self):
         super().__init__('follower_and_avoider')
@@ -35,28 +36,65 @@ class FollowAndAvoid(Node):
 
     def listener_callback(self, msg: Detection2DArray):
         vel = Twist()
+
+        ball_pose = None
+        person_pose = None
+
         for detection in msg.detections:
             for results in detection.results:
                 label = self.id_parser(results.id)
+                cx = detection.bbox.center.position.x
+                area = detection.bbox.size_x * detection.bbox.size_y
+
                 if label == 'ball':
-                    pass
+                    ball_pose = [cx, area]
                 elif label == 'person':
-                    pass 
-                else:
-                    pass
+                    person_pose = [cx, area]
+
+        #  FOLLOW BALL > AVOID PERSON
+        if ball_pose is not None:
+            lin, ang = self.ball_commands(ball_pose)
+        elif person_pose is not None:
+            lin, ang = self.human_commands(person_pose)
+        else:
+            lin, ang = (0.0, 0.0)
+
+        vel.linear.x = float(lin)
+        vel.angular.z = float(ang)
+        self.vel_pub.publish(vel)
+
+
     def human_commands(self, pose):
-        linear_vel, angular_vel = (0,0)
+        error = self.img_center_x - pose
+
+        # Control Proporcional (girar lejos del humano)
+        angular_vel = -(self.person_K_p * error) * 0.002  
+
+        # Retrocede
+        linear_vel = -0.15/max(pose[1], 1.0)  # Más cerca, más rápido retrocede
+
+        return linear_vel, angular_vel
+    
+    def ball_commands(self, pose):
+        error = self.img_center_x - pose
+
+        # Control Proporcional (girar hacia pelota)
+        angular_vel = (self.ball_K_p * error) * 0.002  
+
+        # Avanzar
+        linear_vel = 0.20/max(pose[1], 1.0)  # Más cerca, más rápido avanza
+
         return linear_vel, angular_vel
 
-    def ball_commands(self, pose):
-        linear_vel, angular_vel = (0,0)
-        return linear_vel, angular_vel
         
     def id_parser(self, val):
         if val == 1:
             return 'ball'
         else:
             return 'person'
+        
+
+
 def main(args=None):
     rclpy.init(args=args)
     node = FollowAndAvoid()
